8626994c639172da5cfbfa67bc33de46194e17a0
Tobias <tulamaster15@gmail.com>
2026-04-28 20:31:47 -0300

    fix: change SelectTask return type to int

--- src/TaskFlow/Services/ConsoleHelper.cs
+++ src/TaskFlow/Services/ConsoleHelper.cs
@@ -16,17 +16,19 @@ public class ConsoleHelper
     }
     public static void EndApp(){}
     public static TaskItem CreateTaskFromConsole()=> new TaskItem();
-    public static void SelectTask()
+    public static int SelectTask()
     {
         Console.WriteLine("Por favor, introduzca el ID de la tarea a seleccionar:");
         string input = Console.ReadLine();
         if (int.TryParse(input, out int taskId))
         {
             Console.WriteLine($"Se ha seleccionado la tarea con ID: {taskId}");
+            return taskId;
         }
         else
         {
             Console.WriteLine("Entrada no válida. Por favor ingrese un número entero.");
+            return -1;
         }
     }
     public static void ShowListOfTasks(){}
